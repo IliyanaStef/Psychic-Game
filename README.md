f7127a28a2c666752c572c33d833dce5679bad70
Lili <iliyanastefanova@msn.com>
2018-12-14 16:40:52 -0500

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Psychic-Game
+
+The computer randomly picks a letter, and the user has to guess which letter the computer chose. The player has 11 tries to guess the right letter:
+
+When the player wins, the Wins counter increases and starts the game over again (without refreshing the page).
+
+When the player loses, the Losses counter increases and restarts the game without a page refresh (just like when the user wins).
---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+// Defines initial variables responding to game fields//
+var wins = 0;
+var losses = 0;       
+var guessesLeft = 11;
+var guessed = []
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+// Starts the game upon pressing of any key//
+document.onkeypress = function() {
+
+//Defines additional variables for computer and user guesses//
+var userGuess = event.key;
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+//Defines the conditions and respoding codes to be executed//
+if (alphabet.indexOf(userGuess) > -1) {
+
+    if (userGuess === computerGuess) {
+        wins++;
+        guessesLeft = 11;
+        guessed = [];
+    }
+
+    if (userGuess != computerGuess) {
+        guessed.push(userGuess);
+        guessesLeft --;
+    }
+
+    if (guessesLeft < 1) {
+        guessesLeft = 11;
+        losses ++;
+        guessed = [];   
+    }
+}
+
+//Updates the values of each variable// 
+document.getElementById("wins").innerHTML = ("Wins: " + wins);
+document.getElementById("losses").innerHTML = ("Losses: " + losses);
+document.getElementById("guessesLeft").innerHTML = ("Guesses left: "  + guessesLeft);
+document.getElementById("guessed").innerHTML = ("Your guesses so far: " + guessed);
+
+};
